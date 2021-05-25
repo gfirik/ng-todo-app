c17a1fffc52ff5a3887a99eaf58d1c98c61bc785
Firdavs Gafurjonov <firdavs.gafurjonov@gmail.com>
2021-05-25 23:28:48 +0900

    created and completed

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from './../../models/todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.scss']
+})
+
+export class TodosComponent implements OnInit {
+  [x: string]: any;
+  
+  todos: Todo[]=[];
+  
+  inputTodo:string = "";
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.todos=[
+      {
+        content: 'CB: Final Project',
+        completed: false
+      },
+      {
+        content: 'PoA: 3rd Assignment',
+        completed: false
+      }
+    ]
+  }
+
+  toggleDone(id) {
+    this.todos.map((v, i)=> {
+      if(i==id) v.completed=!v.completed;
+      return v;
+    }) 
+  }
+
+  deleteTodo(id:number){
+    this.todos= this.todos.filter((v,i)=>i !==id);
+  }
+
+  addTodo () {
+    this.todos.push({
+      content: this.inputTodo,
+      completed: false
+    })
+    
+  }
+}
